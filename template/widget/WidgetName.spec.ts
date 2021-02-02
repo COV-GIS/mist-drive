7314404da13d61e31391f6689fc53d78100f69e2
City of Vernonia GIS <gis@vernonia-or.gov>
2021-02-02 15:05:48 -0800

    we'll get a passing test yet

--- template/widget/WidgetName.spec.ts
+++ template/widget/WidgetName.spec.ts
@@ -1,13 +1,7 @@
 import <%name%> from './<%name%>';
 
 describe('cov/widgets/<%name%>', () => {
-  let instance: <%name%>;
-
-  beforeEach(() => {
-    instance = new <%name%>();
-  });
-
-  test('name should be Slagathor', () => {
-    expect(instance.name).toBe('Bill');
+  test('1 should not equal 2', () => {
+    expect(1).not.toBe(2);
   });
 });
